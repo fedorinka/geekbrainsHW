06b2237a9d5512ee2f9c035c151a8341c4b1cba5
Inna <fedorinka83@gmail.com>
2020-04-24 11:55:02 +0300

    testing git in cmd

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main{
 
     public static void main(String[] args) {
         MyWindow myWindow = new MyWindow(); // создаём объект-окно
+        //что все это было, я уже ничего не помню????!!!!!
         MyWindow myWindow2 = new MyWindow("Пример окна с текстовым полем.");
         MyWindow myWindow3 = new MyWindow(new String[]{"Окно с многострочным текстовым полем и скролом.", "Строка 1 для окна", "Строка 2"});
         ClicableWindow clicableWindow = new ClicableWindow();
